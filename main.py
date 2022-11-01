affc8d9d6ea5c0eaccb363031ead0ea39cdb3753
pranavgorantla <111912611+pranavgorantla@users.noreply.github.com>
2022-11-01 12:30:43 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-
-
-numbers = [5, 20, 30, 35, 50]
-
-insval = int(input('Enter the insertion value: '))
-# ******************************
-# Make your Code
-# ******************************
-
-prinnt (numbers)
+myList = [1, 2, 3, 5, 6, 7, 8, 9, 10]
+print("Original list is:", myList)
+element = int(input("Enter a number to insert into the list: "))
+print("The element to be inserted is:", element)
+l = len(myList)
+index = 0
+for i in range(l):
+    if myList[i] > element:
+        index = i
+        break
+myList = myList[:index] + [element] + myList[index:]
+print("The updated list is:", myList)
